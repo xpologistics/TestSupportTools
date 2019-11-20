d8ad1c0ff9900de7458f64bf769d05e5748b9f93
xdabhinav <Dilip.Abhinav@xpo.com>
2019-11-20 16:11:20 +0530

    First commit

---
+++ Xpo.LastMile.TestSupportTool/Xpo.LastMile.TestSupportTool.Client/Controllers/IntegritySupportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xpo.LastMile.TestSupportTool.Client.Controllers
+{
+    [Route("IntegritySupport")]
+    [ApiController]
+    public class IntegritySupportController : ControllerBase
+    {
+        [HttpGet("GetValue/{referenceNumber}")]
+        public string Compare(string referenceNumber)
+        {
+            return referenceNumber;
+        }  
+    }
+}
